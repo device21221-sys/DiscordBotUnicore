eafc9d30621a08c8b98afae557bb51e5e328198c
device21221-sys <device21221@gmail.com>
2025-10-09 00:23:11 +0300

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,13 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ Bot is running!", 200
+    return "Bot is running!"
 
 def run():
-    # Flask буде працювати на порту Render/Replit
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    # Запуск Flask у фоновому потоці, щоб бот не падав
     t = Thread(target=run)
     t.start()
